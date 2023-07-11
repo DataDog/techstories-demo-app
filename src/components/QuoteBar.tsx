e1fb212600a218340d82723bd98bd4d322e5660e
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2023-07-11 17:50:14 -0400

    fixes quotebar

--- src/components/QuoteBar.tsx
+++ src/components/QuoteBar.tsx
@@ -28,8 +28,14 @@ export const QuoteBar = () => {
 
   return (
     <div className="w-full bg-neutral-300">
-      <div className="mx-auto max-w-4xl p-2">
-        {loading ? "Loading..." : error || quote}
+      <div className="mx-auto flex max-w-4xl items-center justify-between px-2 py-4">
+        <div className="italic">{quote}</div>
+        <button
+          onClick={() => void getQuote()}
+          className="rounded-md bg-neutral-100 px-2 py-1 text-sm hover:bg-neutral-500 hover:text-neutral-100"
+        >
+          Get a new quote
+        </button>
       </div>
     </div>
   );
