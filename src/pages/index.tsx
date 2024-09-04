32392acefed0b39e6dc76162fab7643f4750fbdc
Tara S <tara.schoenherr@datadoghq.com>
2024-09-04 12:20:44 -0600

    remove demo account creds from ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,8 @@ const Home: NextPage = () => {
       <p className="w-100 bg-neutral-200 p-4 text-center">
         Welcome to TechStories, new social network for friends write their own
         stories about technology. Log in to create, comment, and vote on
-        stories. This site is in private beta, so no new signups are being
-        accepted at this time. However, you can still log in with the demo
-        account below.
+        stories. This site is in private beta, so no new user signups are being
+        accepted at this time. 
         <br />
         <br />
         <strong>Username:</strong> <code>alice.smith@example.com</code>
