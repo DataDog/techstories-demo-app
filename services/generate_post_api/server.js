92a4afd9961b4cea95dd9c305fb1a6e694c7cb35
Tara S <tara.schoenherr@datadoghq.com>
2024-10-05 23:30:46 -0600

    quotes api wip

---
+++ services/generate_post_api/server.js
@@ -0,0 +1,24 @@
+const tracer = require("dd-trace").init();
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.use(cors());
+
+// Read the JSON file containing posts
+const posts = JSON.parse(fs.readFileSync("demo_posts.json", "utf-8"));
+
+app.get("/generate_post", async (req, res) => {
+  let randomIndex = Math.floor(Math.random() * posts.length);
+
+  // Respond with a random post
+  res.json({ post: posts[randomIndex] });
+});
+
+app.listen(port, () => {
+  console.info(`Server is running at http://localhost:${port}`);
+});
+
