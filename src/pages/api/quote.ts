8cbd4a1e97a4f89d47bd2f08106cd86f9bb80ee9
Tara S <tara.schofield@datadoghq.com>
2025-09-08 14:39:58 -0600

    fix quotes url

--- src/pages/api/quote.ts
+++ src/pages/api/quote.ts
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const data = await authenticatedFetchJson(`${QUOTES_API_URL}/hello`);
+    const data = await authenticatedFetchJson(`${QUOTES_API_URL}/quote`);
     res.status(200).json(data);
   } catch (err) {
     console.error("Quote API error:", err);
